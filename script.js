9e786242c99847f8c38993bcb6fdd22e58286ca5
fallcoder <fallcoder@users.noreply.github.com>
2024-08-31 09:26:27 +0000

    added daily withdrawal feature

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 class BankAccount {
     // constructor initializes the owner and the balance of the account
-    constructor(owner, balance) {
-        this.owner = owner;
-        this.balance = balance;
-        this.transactions = [];
+    constructor(owner, balance, dailyLimit) {
+        this.owner = owner; // account owner
+        this.balance = balance; // account balance
+        this.dailyLimit = dailyLimit; // daily withdrawl limit
+        this.totalWithdrawnToday = 0; // total amount withdrawn today
+        this.transactions = []; // list of transactions
     }
 
     // method to display the account balance 
@@ -11,7 +13,7 @@ class BankAccount {
         console.log("balance: " + this.balance + " EUR");
     }
 
-    // method to display amount into account
+    // deposit money into account
     deposit(amount) {
         console.log("deposit of " + amount + " EUR");
         this.balance += amount;
@@ -19,32 +21,43 @@ class BankAccount {
         this.showBalance();
     }
 
-    // method to withdraw money from the account
+    // withdraw money from the account respecting the daily limit   
     withdraw(amount) {
         // check if the amount to withdraw is greater than the balance
         if(amount > this.balance) {
             console.log("withdrawal denied !");
         }
+        // check if the daily limit is respected
+        else if((this.totalWithdrawnToday + amount) > this.dailyLimit) {
+            console.log("withdrawal denied ! daily withdrawal reached");
+        }
         else {
             console.log("withdrawal of " + amount + " EUR");
             this.balance -= amount;
+            this.totalWithdrawnToday += amount;
             this.transactions.push(`withdrawal of ${amount} EUR`)
             this.showBalance();
         }
     }
 
-    // new: method to get transaction history
+    // reset daily withdrawal total
+    resetDailyWithdrawals() {
+        this.totalWithdrawnToday = 0;
+    }
+
+    // method to get transaction history
     getTransactionHistory() {
         console.log("\ntransactions history")
         this.transactions.forEach(transaction => console.log(transaction))
     }
 }
 
-// create an instance of BankAccount with the owner's name and initial balance
-const myAccount = new BankAccount("Mouhamed", 500);
+// create an instance of BankAccount with the owner's name, initial balance and daily withdrawn limit
+const myAccount = new BankAccount("Mouhamed", 500, 200);
 console.log(myAccount)
-myAccount.showBalance();
-myAccount.deposit(100)
-myAccount.withdraw(200)
-myAccount.getTransactionHistory()
+myAccount.showBalance(); // display the initial account balance
+myAccount.deposit(100) // amount deposit
+myAccount.withdraw(200) // withdraw allowed since it's under the limit
+myAccount.withdraw(250) // withdraw declined since it exceeds the daily limit
+myAccount.getTransactionHistory() // display the transactions history
 
